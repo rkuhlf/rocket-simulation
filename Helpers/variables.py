f67384ab085bf84c79c961bce28a6477558c07c6
Riley Kuhlman <rilstarssun@gmail.com>
2021-02-26 15:44:57 -0600

    Fixed wierd global errors with previous values

--- Helpers/variables.py
+++ Helpers/variables.py
@@ -1,22 +1,28 @@
+# These are basically the globabl variables that all files need access to
+
+
 import numpy as np
 import pandas as pd
 
 # INITIAL
 position = np.array([0, 0], dtype="float64")
-p_position = np.copy(position)
 velocity = np.array([0, 0], dtype="float64")
-p_velocity = np.copy(velocity)
+
+# the acceleration isn't even necessary, but I do have to declare the previous value, so I'll just leave it here for clarity
 acceleration = np.array([0, 0], dtype="float64")
-p_acceleration = np.copy(acceleration)
+
+# indicates whether the rocket has begun to fall
+turned = False
+
 
 mass = 5.76  # kg
 t = 0  # seconds
 
 
 # CONSTANTS
-time_increment = 0.01 # seconds
-earth_mass = 5.972 * 10 ** 24 # kg
-earth_radius = 6371071.03 # meters
+time_increment = 0.01  # seconds
+earth_mass = 5.972 * 10 ** 24  # kg
+earth_radius = 6371071.03  # meters
 gravitational_constant = 6.67 * 10 ** -11
 
 # Lake Jackson altitude
--- rocket.py
+++ rocket.py
@@ -9,27 +9,17 @@
 # TODO: three degree of freedom - in x, y, and z modelling
 # Model rotation in x, y, and z
 
+from Helpers.dragForce import *
+from Helpers.gravity import *
+from Helpers.thrust import *
+from Helpers.general import interpolate
+
 # also includes any libraries that are imported in this file
 from Helpers.variables import *
-from Helpers.general import interpolate
-from Helpers.thrust import *
-from Helpers.gravity import *
-from Helpers.dragForce import *
 
-
-
-# Calculate using http://www.rasaero.com/dl_software_ii.htm
-drag_coefficient = 0.75  # double check that it would be the same both up and sideways
-# TODO: Find real data for areas
-vertical_area = 0.01  # m^2
-sideways_area = 0.1  # m^2
-area = np.array([sideways_area, vertical_area])
-
-# indicates whether the rocket has begun to fall
-turned = False
-
-output = open("Data/Output/output.csv", "w")
-output.write("x,y,x_acc,y_acc\n")
+p_position = np.copy(position)  # p stands for previous
+p_velocity = np.copy(velocity)
+p_acceleration = np.copy(acceleration)
 
 
 def log_data(file, data):
@@ -38,13 +28,10 @@ def log_data(file, data):
     file.write("\n")
 
 
-print("Launching rocket")
-while position[1] >= 0:
-    log_data(
-        output, [position[0],
-                 position[1],
-                 acceleration[0],
-                 acceleration[1]])
+def simulate_step():
+    # acceleration has to be global so that it can be logged
+    global t, mass, velocity, position, acceleration
+
     t += time_increment
 
     # You have to reset acceleration every time
@@ -55,12 +42,13 @@ while position[1] >= 0:
 
 
     thrust = get_thrust(t - time_increment / 2)
-    force += thrust
+    # adjust this for the angle
+    force[1] += thrust
 
     new_mass = mass - thrust * mass_per_thrust * time_increment
-
     # adjust for momentum
     velocity *= mass / new_mass
+
 
     mass = new_mass
 
@@ -74,13 +62,34 @@ while position[1] >= 0:
 
     # Taking the average of the current acceleration and the previous acceleration is a better approximation of the change over the time_interval
     # It is basically a riemann midpoint integral over the acceleration so that it is more accurate finding the change in velocity
-    combined_acceleration = (p_acceleration + acceleration) / 2
-
+    combined_acceleration = (
+        p_acceleration + acceleration) / 2
     velocity += acceleration * time_increment
 
     combined_velocity = (p_velocity + velocity) / 2
-
     position += combined_velocity * time_increment
+
+
+# Calculate using http://www.rasaero.com/dl_software_ii.htm
+drag_coefficient = 0.75  # double check that it would be the same both up and sideways
+# TODO: Find real data for areas
+vertical_area = 0.01  # m^2
+sideways_area = 0.1  # m^2
+area = np.array([sideways_area, vertical_area])
+
+
+output = open("Data/Output/output.csv", "w")
+output.write("x,y,x_acc,y_acc\n")
+
+
+print("Launching rocket")
+while position[1] >= 0:
+    simulate_step()
+    log_data(
+        output, [position[0],
+                 position[1],
+                 acceleration[0],
+                 acceleration[1]])
 
     # y is the second index
     if not turned and position[1] < p_position[1]:
@@ -88,6 +97,10 @@ while position[1] >= 0:
             t, position[1]))
         turned = True
 
-    update_previous()
+    p_position = np.copy(position)
+    p_velocity = np.copy(velocity)
+    p_acceleration = np.copy(acceleration)
+
+
 
 output.close()
