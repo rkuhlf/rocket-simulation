546d074a53dfd1b6f08a5ab853e8558a3108552c
Riley Kuhlman <rilstarssun@gmail.com>
2021-08-29 10:10:56 -0500

    Demonstrate strange behavior in time delta study

---
+++ Simulations/TimeDeltaStudy.py
@@ -0,0 +1,58 @@
+import sys
+
+sys.path.append(".")
+
+
+
+from Helpers.general import angles_from_vector_3d
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import array
+from simulation import Simulation
+from logger import Feedback_Logger  # , Logger
+from RocketParts.parachute import ApogeeParachute
+from rocket import Rocket
+from RocketParts.motor import Motor
+from environment import Environment
+
+
+
+
+
+# Just using apogee as an indicator of convergence
+
+
+# Uses mostly class defaults. No wind so I can compare them
+# Notice that it is a 17 second sim to apogee
+# just curious to see the behavior over the higher values
+
+def frange(x, y, jump):
+    while x < y:
+        yield x
+        x += jump
+
+
+time_deltas = list(frange(0.0001, 0.01, jump=0.0001)) + list(frange(0.01, 0.1, jump=0.01)) + list(frange(0.1, 3, jump=0.1))
+apogees = []
+for time_delta in time_deltas:
+    env = Environment({"time_increment": time_delta, "apply_wind": False})
+    motor = Motor()
+    rocket = Rocket(environment=env, motor=motor, parachutes=[])
+
+    sim = Simulation(
+        {"apply_angular_forces": True, "max_frames": -1,
+         "stopping_errors": False},
+        env, rocket)
+    sim.run_simulation()
+
+    apogee = sim.apogee()
+    apogees.append(apogee)
+
+    print(time_delta)
+    print(apogee)
+
+print(time_deltas)
+print(apogees)
+
+plt.plot(time_deltas, apogees)
+plt.show()
