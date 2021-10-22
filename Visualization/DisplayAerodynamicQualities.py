9df8bd3581d112744f7f9defcfb86b4625ea3bba
Riley <rileyk42@icloud.com>
2021-10-22 11:05:36 -0500

    Update and rename displayAerodynamicQualities.py to DisplayAerodynamicQualities.py

--- Visualization/DisplayAerodynamicQualities.py
+++ Visualization/DisplayAerodynamicQualities.py
@@ -1,17 +1,15 @@
+# DISPLAY AERODYNAMIC DATA OVER MACH
+# Looking over mach numbers should give the same information as looking over Reynolds numbers
 # Several graphs intended to illustrate how different inputs affect the output of coefficient of drag and center of pressure
 
-import sys
-# This adds the project to te path so that I can import helpers
-sys.path.append(".")
-# Depending on how aggressive any autoformatting is it may be necessary to save this without formatting so that the path is affected at the start
-
-from Helpers.general import numpy_from_string
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
+import sys
+sys.path.append(".")
 
-
+from Helpers.general import numpy_from_string
 
 
 # Files are relative to the project folder you are running in, not the file location
@@ -23,5 +21,7 @@ data = pd.read_csv("Data/Input/aerodynamicQualities.csv")
 data.plot.line(x='Mach', y='CD')
 # data.plot.line(x='time', y='g-force')
 # data.plot.line(x='time', y='mock')
+
+# TODO: show 10-odd isolines for different angles of attack over mach numbers
 
 plt.show()
