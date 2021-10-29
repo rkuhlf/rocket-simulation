07d31f0d6653f7fd126a13d5d2bb8dddbcf10af7
Riley <rileyk42@icloud.com>
2021-10-29 09:53:14 -0500

    Update ThrustProfile.py

--- Data/Input/ThrustProfile.py
+++ Data/Input/ThrustProfile.py
@@ -5,37 +5,42 @@
 
 import pandas as pd
 
-data = pd.read_csv("./Data/Input/thrustCurveO6300.csv")
-
-desired_burn_time = 22
-desired_average_thrust = 10000
 
 
 
+def scale_curve(data, desired_burn_time, desired_average_thrust):
+    times = list(data["time"])
 
-times = list(data["time"])
+    burn_time = times[-1]
 
-burn_time = times[-1]
+    thrusts = list(data["thrust"])
 
-thrusts = list(data["thrust"])
+    total_impulse = 0
 
-total_impulse = 0
-
-# Don't worry too much about total impulse; it has extremely high variability between runs of the motor and depending on how you count the in betweens you can get a range of 5000 Ns
-for i in range(len(times) - 1):
-    # Use a trapezoidal reimann sum to approximate the integral
-    total_impulse += (times[i + 1] - times[i]) * (thrusts[i] + thrusts[i + 1]) / 2
-
-
-
-average_thrust = total_impulse / burn_time
+    # Don't worry too much about total impulse; it has extremely high variability between runs of the motor and depending on how you count the in betweens you can get a range of 5000 Ns
+    for i in range(len(times) - 1):
+        # Use a trapezoidal reimann sum to approximate the integral
+        total_impulse += (times[i + 1] - times[i]) * (thrusts[i] + thrusts[i + 1]) / 2
 
 
-data["time"] *= desired_burn_time / burn_time
-data["thrust"] *= desired_average_thrust / average_thrust
 
-print(data)
+    average_thrust = total_impulse / burn_time
 
-data.set_index("time", inplace=True)
 
-data.to_csv("./Data/Input/currentGoddard.csv")
+    data["time"] *= desired_burn_time / burn_time
+    data["thrust"] *= desired_average_thrust / average_thrust
+    
+    return data
+
+
+if __name__ == "__main__":
+    data = pd.read_csv("./Data/Input/thrustCurveO6300.csv")
+
+    data = scale_curve(data, 22, 10000)
+    
+    print(data)
+    
+    data.set_index("time", inplace=True)
+
+    data.to_csv("./Data/Input/currentGoddard.csv")
+    
