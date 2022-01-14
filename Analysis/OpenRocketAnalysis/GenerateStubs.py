819486ed2d0b2879ac12b9a5fcf6124bd242d3c5
Riley Kuhlman <rilstarssun@gmail.com>
2022-01-13 18:30:32 -0600

    Thought of ideas for other computer

--- Analysis/OpenRocketAnalysis/GenerateStubs.py
+++ Analysis/OpenRocketAnalysis/GenerateStubs.py
@@ -1,3 +1,37 @@
+
+# OG
+# Creates autocompletion for openrocket
+# I have hidden the stubs file that is generated in my .vscode settings
+# 
+
+import jpype
+import stubgenj
+
+
+jvm_path = r"C:\Program Files\Java\jdk1.8.0_251\jre\bin\server\jvm.dll"
+jar_path = r"C:\Users\riley\AppData\Local\OpenRocket\app\OpenRocket-15.03.jar"
+
+jpype.startJVM(jvm_path, "-ea", f"-Djava.class.path={jar_path}")
+
+import jpype.imports # any java imports have to come after this imports import
+from java import io
+
+# import java.util  # type: ignore
+import java.io.File # type: ignore
+
+# from net.sf.openrocket import simulation # type: ignore
+
+# jpype.JPackage("net").sf.openrocket
+stubgenj.generateJavaStubs([jpype.JPackage("net").sf.openrocket], useStubsSuffix=True)
+
+
+
+
+
+
+
+
+
 # # Creates autocompletion for openrocket
 # # I have hidden the stubs file that is generated in my .vscode settings
 # # 
@@ -10,7 +44,7 @@ from Helpers.data import load_environment_variable
 # # from java import io
 # # io.
 jvm_path = load_environment_variable("JVM_PATH")
-jar_path = load_environment_variable("OR_PATH")
+jar_path = load_environment_variable("OR_JAR_PATH")
 
 # jpype.startJVM(jvm_path, "-ea", f"-Djava.class.path={jar_path}")
 
@@ -37,6 +71,7 @@ import stubgenj
 jpype.startJVM(jvm_path, "-ea", r"-Djava.class.path=C:\Users\rkuhlman813\Downloads\openrocket-unstable\openrocket-unstable\core\src\net\sf\openrocket")
 import jpype.imports  # noqa
 # from jpype.JPackage("net").sf.openrocket import simulation  # noqa
+# maybe also try a classpath argument with a list of paths: classpath=['lib/*', 'classes']
 
 stubgenj.generateJavaStubs([jpype.JPackage("net").sf.openrocket], useStubsSuffix=True)
 
@@ -44,3 +79,5 @@ stubgenj.generateJavaStubs([jpype.JPackage("net").sf.openrocket], useStubsSuffix
 
 # Maybe it is supposed to be a JClass with the local installation of the openrocket source
 # C:\Users\rkuhlman813\Downloads\openrocket-unstable\openrocket-unstable\core\src\net\sf\openrocket\aerodynamics\Warning.java
+
+# I think that if you put the openrocket stuff in the same folder as the python script, you can use the jpype.imports functionality to allow you to import it
