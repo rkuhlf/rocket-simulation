854087b099a22415cf0c67123edd565fd938ef50
Riley Kuhlman <rilstarssun@gmail.com>
2022-01-30 19:41:29 -0600

    Update drag

--- Analysis/GraphPercentiles.py
+++ Analysis/GraphPercentiles.py
@@ -95,8 +95,8 @@ if __name__ == "__main__":
     from Helpers.visualization import make_matplotlib_big
     make_matplotlib_big()
 
-    # plot_distribution("altitude", title="Distribution of Altitude", axis_label="Altitude (ft)")
-    # plot_distribution("velocity", title="Distribution of Velocity", axis_label="Velocity (ft/s)")
+    plot_distribution("altitude", title="Distribution of Altitude", axis_label="Altitude (ft)")
+    plot_distribution("velocity", title="Distribution of Velocity", axis_label="Velocity (ft/s)")
     plot_distribution("drag", title="Distribution of Drag", axis_label="Drag (lbs)")
     plot_distribution("thrust", title="Simulated Thrust Curves", axis_label="Thrust (lbs)")    
     
