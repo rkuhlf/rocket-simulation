9b0b646cca6f616a858e9e72c9194c9b6d268d05
rkuhlf <rilstarssun@gmail.com>
2022-03-24 09:03:04 -0500

    Set up flight monte carlo

--- Analysis/OpenRocketAnalysis/FlightMonteCarlo.py
+++ Analysis/OpenRocketAnalysis/FlightMonteCarlo.py
@@ -3,8 +3,6 @@
 # I think I should be more careful with this. All of the units have to be base metric for this to work
 
 import pandas as pd
-import numpy as np
-from random import gauss
 import javaInitialization
 from Analysis.OpenRocketAnalysis.monteCarloFlightOR import MonteCarloFlightOR, MonteCarloFlightRandomMotorOR
 from Analysis.monteCarlo import create_motors_from_directory
@@ -12,28 +10,23 @@ from Analysis.monteCarlo import create_motors_from_directory
 from Analysis.OpenRocketAnalysis.openRocketHelpers import apogee, most_updated_sim, new_or_instance
 
 
-# Simulation threw error single positional indexer is out-of-bounds
-# Might be because it goes above mach 3. I cannot remember if I am also looking up from the AOA.
-# Might be the air density data running out.
-# Might be some motor data running out. FIXME: pretty important iff it keeps coming up
-
 if __name__ == '__main__':
     import orhelper
     from orhelper import Helper
 
     with new_or_instance() as instance:
         orh = Helper(instance)
-        motors = create_motors_from_directory("./Analysis/MotorMonteCarloCorrectCGs/", 100)
+        motors = create_motors_from_directory("./Analysis/MotorMonteCarloUpdatedDimensions/", 100)
 
         df = pd.read_csv("./Data/Input/shorterOxTankAerodynamics.csv")
-        m = MonteCarloFlightRandomMotorOR(orh, motors, drag_dataframe=df, dry_mass=50.2, dry_CG=3.404, ox_tank_front=1.600)
+        m = MonteCarloFlightRandomMotorOR(orh, motors, drag_dataframe=df, dry_mass=49.5, dry_CG=3.43, ox_tank_front=1.625)
 
         
         m.simulate_randomized(100)
 
     m.print_characteristic_figures()
 
-    root_folder = "./Analysis/ShortenedRocketFlight-Temporary"
+    root_folder = "./Analysis/MonteCarloPreDatcom"
     m.save_important_data(f"{root_folder}/MonteCarloFlightSimulations/")
 
     m.save_characteristic_figures(f"{root_folder}/MonteCarloFlightData/output.csv")
