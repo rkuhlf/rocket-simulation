1f72b79286cc069707c9d6b06a432772d7ece8ec
Riley Kuhlman <rileyk2@icloud.com>
2023-01-25 15:11:34 -0600

    Sort visualization

---
+++ doc/example.md
@@ -0,0 +1,8 @@
+
+
+
+## Organization of Example
+
+I only included things in visualization if they were not running any kind of simulation. Otherwise they belong in analysis.
+
+Analysis is for things that run lots of simulations, simulation is for those that are mainly running one sim.
--- example/visualization/input_analysis/comparefittedaccuracy.py
+++ example/visualization/input_analysis/comparefittedaccuracy.py
@@ -2,54 +2,51 @@
 # Display the input points along with the fitted line to match them
 # It's important to visually verify that we aren't getting significant error in the inputs
 
-import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from src.constants import aero_path, atmosphere_path
 
 from src.data.input.models import *
 
 
-def display_density():
-    # Files are relative to the project folder you are running in, not the file location
-    data = pd.read_csv("Data/Input/airQuantities.csv")
+def plot_density_comparison():
+    data = pd.read_csv(f"{atmosphere_path}/airQuantities.csv")
 
     inputs = np.arange(0, 20, 0.1)
     outputs = get_density(inputs)
 
-    src.data.plot.scatter(x='Altitude', y='Density')
+    data.plot.scatter(x='Altitude', y='Density')
     plt.plot(inputs, outputs, 'r')
 
-    plt.show()
 
+def plot_aero_comparison():
+    data = pd.read_csv(f"{aero_path}/aerodynamicQualities.csv")
 
-def display_aero():
-    data = pd.read_csv("Data/Input/aerodynamicQualities.csv")
-
-    # Rasaero outputs to a max of 21
+    # Rasaero outputs to a max mach of 21
     inputs = np.arange(0, 25, 0.1)
 
     outputs = list(map(get_splined_coefficient_of_drag, inputs))
     # outputs = get_coefficient_of_drag(inputs)
 
-    src.data.plot.scatter(x='Mach', y='CD')
+    data.plot.scatter(x='Mach', y='CD')
     plt.plot(inputs, outputs, 'r')
 
-    plt.show()
 
-
-def display_mach():
-    data = pd.read_csv("Data/Input/airQuantities.csv")
+def plot_mach_comparison():
+    data = pd.read_csv(f"{atmosphere_path}/airQuantities.csv")
 
     inputs = np.arange(0, 40, 0.1)
 
     outputs = list(map(get_speed_of_sound, inputs))
 
-    src.data.plot.scatter(x='Altitude', y='Speed of sound')
+    data.plot.scatter(x='Altitude', y='Speed of sound')
     plt.plot(inputs, outputs, 'r')
-
-    plt.show()
 
 
 if __name__ == "__main__":
-    display_density()
+    plot_density_comparison()
+    plot_aero_comparison()
+    plot_mach_comparison()
+
+    plt.show()
